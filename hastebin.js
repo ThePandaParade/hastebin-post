e1673e0958053d815824adb33e3d7c058df39253
Main_User_Drive <hi.pixelpikachu@hotmail.com>
2018-12-04 19:58:21 +0000

    typo

--- hastebin.js
+++ hastebin.js
@@ -3,6 +3,6 @@ exports.post = async function(string, options) {
     if (string === undefined) {throw "String is a required argument"}
     const req = await superagent.post('https://www.hastebin.com/documents')
      .send(string)
-     .catch((err) => {throw "Failed to "})
+     .catch((err) => {throw "Failed to post to Hastebin. Hastebin might be down, or your internet is dead. Please try again"})
     return `https://hastebin.com/${req.body.key}` 
 }
