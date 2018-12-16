6645c28ce3471723bd8e1bfc13df601796c363a7
Vilgot <35927380+Vilgot@users.noreply.github.com>
2018-12-16 17:44:15 +0100

    Update hastebin.js

--- hastebin.js
+++ hastebin.js
@@ -1,5 +1,5 @@
 const superagent = require('superagent')
-exports.post = async function(string, options) {
+module.exports = async function(string, options) {
     if (string === undefined) {throw "String is a required argument"}
     const req = await superagent.post('https://www.hastebin.com/documents')
      .send(string)
